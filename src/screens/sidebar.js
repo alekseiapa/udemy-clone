ad658248e6b5a1baea6b4e7e8a8226749c7e0f3f
Aleksei <aapaseev@gmail.com>
2021-12-23 01:06:01 +0800

    sidebar: added navbar

--- src/screens/sidebar.js
+++ src/screens/sidebar.js
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../ui/logo-udemy.svg";
 
 function Sidebar() {
+  const [nav, setNav] = useState([
+    {
+      label: "Home",
+      slug: "/",
+      icon: "icon-home",
+    },
+    {
+      label: "Dicsover",
+      slug: "discover",
+      icon: "icon-list2",
+    },
+    {
+      label: "Categories",
+      slug: "categories",
+      icon: "icon-price-tag",
+    },
+    {
+      label: "My courses",
+      slug: "courses",
+      icon: "icon-briefcase",
+    },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState("/");
+
+  var navigation = [];
+
+  for (let i = 0; i < nav.length; i++) {
+    navigation.push(
+      <li key={"nav-" + i + "-" + nav[i].slug}>
+        <a
+          href={nav[i].slug}
+          className={
+            "link noul flex c333" + (currentPage === nav[i].slug ? " on" : "")
+          }
+        >
+          <div className={"ico s20 " + nav[i].icon} />
+          <h2 className="lbl s20">{nav[i].label}</h2>
+        </a>
+      </li>
+    );
+  }
+
   return (
     <div className="sidebar fixed">
       <a href="#" className="logo bl">
         <img src={logo} className="bl" />
       </a>
-
-      <ul className="nav">
-        <li>
-          <a href="#">
-            <div className="ico icon-home" />
-            <h2 className="lbl">Home</h2>
-          </a>
-        </li>
-      </ul>
+      <ul className="nav">{navigation}</ul>
     </div>
   );
 }
